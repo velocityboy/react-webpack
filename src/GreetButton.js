2ff3f5d6e2ee39e429a6795bd75515c1e19eedbf
Jim Geist <jimge@rodentia.net>
2017-03-12 09:36:46 -0700

    Fix license headers and LICENSE.txt

--- src/GreetButton.js
+++ src/GreetButton.js
@@ -1,16 +1,8 @@
 /**
 * Copyright (C) 2017 by Jim Geist
 *
-* Permission to use, copy, modify, and/or distribute this software for any purpose
-* with or without fee is hereby granted.
-*
-* THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
-* REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
-* FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
-* INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
-* OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
-* TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
-* THIS SOFTWARE.
+* This software is licensed BSD0 per the contents of the file LICENSE.txt in
+* the root of the repository.
 */
 
 import React, { PropTypes } from 'react';
